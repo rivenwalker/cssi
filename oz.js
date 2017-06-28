a972a527c1372f397a5a035b6aa96f147cb7f56b
austin <rivenwalker@gmail.com>
2017-06-28 17:18:56 -0400

    add listeners

---
+++ oz.js
@@ -0,0 +1,22 @@
+console.log("did this work better now")
+
+var cookies = [
+  "chocolate chip",
+  "the lemon ones",
+  "thin mints",
+  "oreos",
+  "samoa",
+  "oatmeal"
+];
+
+function eat(cookies) {
+  console.log('i just ate '+ cookies);
+}
+var eatbutton = $(".devour");
+eatbutton.on("click",)
+function eatcookies() {
+
+for (var i = 0; i < cookies.length; i = i + 1) {
+  eat(cookies[i]);
+}
+}
